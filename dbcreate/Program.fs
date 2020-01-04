b3e8da1c34840e2186c9034648f0d840f6aaec9e
neilmarshall <neil.marshall@dunelm.org.uk>
2020-01-04 17:07:30 +0000

    move to functional dependency injection approach

--- dbcreate/Program.fs
+++ dbcreate/Program.fs
@@ -7,14 +7,22 @@
         | Info msg -> printfn "INFO: %s" msg
         | Error msg -> printfn "ERROR: %s" msg
 
-module PostgresDBExecutor =
+
+[<AutoOpen>]
+module DomainModel =
+    type DataExtractor<'T> = string -> seq<'T>
+    type ScriptExecutor = string -> Async<unit>
+    type DataWriter = string -> Map<string, obj> -> Async<unit>
+
+
+module PostgresDataManager =
 
     open System.Configuration
     open Npgsql
 
     let private ConnString = ConfigurationManager.ConnectionStrings.["DefaultConnection"].ConnectionString
 
-    let getColumn<'T> query =
+    let getColumn<'T> : DataExtractor<'T> = fun query ->
         try
             seq {
                 use conn = new NpgsqlConnection(ConnString)
@@ -30,7 +38,19 @@ module PostgresDBExecutor =
         | :? System.Data.Common.DbException as err ->
             err.Message |> sprintf "error occurred accessing database: %s" |> failwith
 
-    let insertData (query : string) (parameters : Map<string, obj>) : Async<unit> =
+    let executeScript : ScriptExecutor = fun query ->
+        async {
+            try
+                use conn = new NpgsqlConnection(ConnString)
+                do! conn.OpenAsync() |> Async.AwaitTask
+                use command = new NpgsqlCommand(query, conn)
+                do! command.ExecuteNonQueryAsync() |> Async.AwaitTask |> Async.Ignore
+            with
+            | :? System.Data.Common.DbException as err ->
+                err.Message |> sprintf "error occurred accessing database: %s" |> failwith
+        }
+
+    let insertData : DataWriter = fun query parameters ->
         async {
             try
                 use conn = new NpgsqlConnection(ConnString)
@@ -42,70 +62,65 @@ module PostgresDBExecutor =
             | :? System.Data.Common.DbException as err ->
                 err.Message |> sprintf "error occurred accessing database: %s" |> failwith
         }
+        
 
-    let executeScript (query : string) : Async<unit> =
-        async {
-            try
-                use conn = new NpgsqlConnection(ConnString)
-                do! conn.OpenAsync() |> Async.AwaitTask
-                use command = new NpgsqlCommand(query, conn)
-                do! command.ExecuteNonQueryAsync()|> Async.AwaitTask |> Async.Ignore
-            with
-            | :? System.Data.Common.DbException as err ->
-                err.Message |> sprintf "error occurred accessing database: %s" |> failwith
-        }
-
-
-module Scripts =
+module ScriptExecutor =
 
     open System.IO
 
-    let private getExecutedScripts (extractor : string -> seq<'T>) : seq<'T> =
-        seq { yield! extractor "SELECT filename FROM version;" }
-
-    let private getUnexecutedScripts (extractor : string -> seq<'T>) directory =
-        let allScripts = Directory.GetFiles(directory) |> Set.ofSeq
-        let executedScripts =
-            getExecutedScripts extractor
-            |> Seq.map (fun filename -> Path.Combine([|directory; filename|]))
-            |> Set.ofSeq
-        Set.difference allScripts executedScripts
-
-    let private writeFilenameAsync (writer : string -> Map<string, obj> -> Async<unit>) (filename : string) =
-        async {
-            let query = "INSERT INTO version (filename, date) VALUES (@filename, @date);"
-            let parameters = Map.ofList ["filename", box(filename); "date", box(System.DateTime.Now)]
-            do! writer query parameters |> Async.Ignore
-        }
-
     let executeScriptsAsync
-            (extractor : string -> seq<'T>) 
-            (scriptExecutor : string -> Async<unit>)
-            (writer : string -> Map<string, obj> -> Async<unit>)
+            (extractor : DataExtractor<string>) 
+            (scriptExecutor : ScriptExecutor)
+            (writer : DataWriter)
             (directory : string)
                 : Async<unit []> =
+
+        let getUnexecutedScripts directory =
+            let getExecutedScripts =
+                seq { yield! extractor "SELECT filename FROM version;" }
+            let allScripts = Directory.GetFiles(directory) |> Set.ofSeq
+            let executedScripts =
+                getExecutedScripts
+                |> Seq.map (fun filename -> Path.Combine([|directory; filename|]))
+                |> Set.ofSeq
+            Set.difference allScripts executedScripts
+
         let executeScriptAsync filename =
+            let writeFilenameAsync filename =
+                async {
+                    let query = "INSERT INTO version (filename, date) VALUES (@filename, @date);"
+                    let parameters = Map.ofList ["filename", box(filename); "date", box(System.DateTime.Now)]
+                    do! writer query parameters |> Async.Ignore
+                }
             async {
+                filename |> sprintf "Executing script '%s'" |> Logging.Info |> Logging.logMessage
                 let! script = File.ReadAllTextAsync(filename) |> Async.AwaitTask
                 do! scriptExecutor script |> Async.Ignore
-                do! writeFilenameAsync writer (Path.GetFileName(filename)) |> Async.Ignore
+                do! Path.GetFileName(filename) |> writeFilenameAsync |> Async.Ignore
            }
-        directory |> getUnexecutedScripts extractor |> Seq.map executeScriptAsync |> Async.Parallel
+        directory |> getUnexecutedScripts |> Seq.map executeScriptAsync |> Async.Parallel
 
 
 module ComputationRoot =
 
-    let scriptExecutor = Scripts.executeScriptsAsync PostgresDBExecutor.getColumn PostgresDBExecutor.executeScript PostgresDBExecutor.insertData
+    let scriptExecutor =
+        ScriptExecutor.executeScriptsAsync
+            PostgresDataManager.getColumn
+            PostgresDataManager.executeScript
+            PostgresDataManager.insertData
 
 
 module Program =
 
+    open System.Reflection
+
     [<EntryPoint>]
     let main argv =
         Logging.Info "Commencing DataBase creation" |> Logging.logMessage
-        let basePath = System.IO.FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).Directory.FullName
+        let basePath = System.IO.FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName
         System.IO.Path.Combine([|basePath; "scripts"|])
         |> ComputationRoot.scriptExecutor
+        |> Async.RunSynchronously
         |> ignore
         Logging.Info "DataBase creation completed" |> Logging.logMessage
         0 // return an integer exit code
