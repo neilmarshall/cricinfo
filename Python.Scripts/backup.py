15dc4ed90681ea01d3f0b021f7c03f51d73c07af
neilmarshall <neil.marshall@dunelm.org.uk>
2020-02-09 19:02:36 +0000

    add backup script

---
+++ Python.Scripts/backup.py
@@ -0,0 +1,20 @@
+#! venv/bin/python3
+
+import os
+import datetime
+import subprocess
+import sys
+
+BASE_DIR = "backups"
+
+if not os.path.exists(BASE_DIR):
+    os.mkdir(BASE_DIR)
+
+filename = os.path.join(os.getcwd(), BASE_DIR, f"{datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}.sql")
+
+if sys.platform == 'win32':
+    pass
+elif sys.platform == 'darwin':
+    subprocess.run(["/Library/PostgreSQL/11/bin/pg_dump", "-d", "cricinfo", "-f", filename])
+else:
+    raise OSError("OS not supported")
