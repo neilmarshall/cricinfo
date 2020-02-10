6e919d8f844e1200b8703de92ce02cdb16011f7f
Neil Marshall <Neil.Marshall@riskfirst.com>
2020-02-10 08:42:01 +0000

    fix issues with DB deploy scripts

--- Db.Deploy/scripts/cricinfo_001.sql
+++ Db.Deploy/scripts/cricinfo_001.sql
@@ -120,3 +120,19 @@ CREATE TABLE fall_of_wicket_scorecard (
 	CONSTRAINT runs_non_negative CHECK (runs >= 0),
 	CONSTRAINT wickets_non_negative CHECK (wickets >= 0)
 );
+
+CREATE OR REPLACE VIEW show_matches ("Id", "Match Type", "Date", "Home Team", "Away Team", "Result")
+AS
+    SELECT m.id, mt.type, m.date_of_first_day, ht.name, at.name, r.type
+    FROM match m
+    JOIN match_type mt
+    ON m.match_type_id = mt.id
+    JOIN venue v
+    ON m.venue_id = v.id
+    JOIN team ht
+    ON m.hometeam_id = ht.id
+    JOIN team at
+    ON m.awayteam_id = at.id
+    JOIN result r
+    ON m.result_id = r.id
+    ORDER BY m.date_of_first_day;
