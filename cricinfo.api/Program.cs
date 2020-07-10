9d0e0ef90ea59080c2e6992ac6c30b67c99b0542
Neil Marshall <Neil.Marshall@riskfirst.com>
2020-07-10 14:26:20 +0100

    configure Azure logging

--- cricinfo.api/Program.cs
+++ cricinfo.api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.AzureAppServices;
 
 namespace Cricinfo.Api
 {
@@ -21,6 +22,7 @@ namespace Cricinfo.Api
                 .ConfigureLogging(logging =>
                 {
                     logging.AddConsole();
+                    logging.AddAzureWebAppDiagnostics();
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
