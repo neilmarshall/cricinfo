ffc9e8066e4b7a6dd6b018dd5655685ee48c9dd6
Neil Marshall <Neil.Marshall@riskfirst.com>
2020-09-07 17:41:33 +0100

    improve quality and extend coverage of integration tests
    Squashed commit of the following:
    
    commit 4c7225f63162610f689076be13ae301c63acb267
    Author: Neil Marshall <Neil.Marshall@riskfirst.com>
    Date:   Mon Sep 7 16:38:19 2020 +0100
    
        add 'england-pakistan-1-9-20.json' test file
    
    commit 735072019c76faa22604c77af5833ac1ed1333c6
    Author: Neil Marshall <Neil.Marshall@riskfirst.com>
    Date:   Mon Sep 7 17:17:06 2020 +0100
    
        fix up issues with SA v. England 26-12-19 test data
    
    commit 0c99b0d9b35fe0fac11d08246ade81b20c4bcb27
    Author: Neil Marshall <Neil.Marshall@riskfirst.com>
    Date:   Mon Sep 7 17:05:42 2020 +0100
    
        update test for small glitches
    
    commit 9a59bde6cd99c27483a3be9831b7cb4b982ceb95
    Author: Neil Marshall <Neil.Marshall@riskfirst.com>
    Date:   Mon Sep 7 16:38:19 2020 +0100
    
        add 'england-australia-6-9-20.json' test file
    
    commit de916bc439a74649d7637f1b1cf4003f64fb6481
    Author: Neil Marshall <Neil.Marshall@riskfirst.com>
    Date:   Mon Sep 7 16:35:13 2020 +0100
    
        delete and recreate match as part of test
    
    commit f5a9b58e81320726d3426ac281b92c6e21d7cebb
    Author: Neil Marshall <Neil.Marshall@riskfirst.com>
    Date:   Mon Sep 7 16:26:23 2020 +0100
    
        parameterise filepath and move inside test method

--- Tests/Cricinfo.Integration.Tests/ICricInfoQueryServiceFixtures.cs
+++ Tests/Cricinfo.Integration.Tests/ICricInfoQueryServiceFixtures.cs
@@ -13,25 +13,38 @@ namespace Cricinfo.Integration.Tests
     [TestClass]
     public class ICricInfoQueryServiceFixtures
     {
+        private static ICricInfoCommandService cricInfoCommandService;
         private static ICricInfoQueryService cricInfoQueryService;
-        private static Match expected;
 
         [ClassInitialize]
         public static void Initialize(TestContext _)
         {
             var dbConnectionString = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location).ConnectionStrings.ConnectionStrings["DbConnectionString"].ConnectionString;
+            cricInfoCommandService = new CricInfoCommandService<Match>(dbConnectionString);
             cricInfoQueryService = new CricInfoQueryService<Match>(dbConnectionString);
-
-            var a = Assembly.GetExecutingAssembly();
-            using Stream s = a.GetManifestResourceStream("Cricinfo.Integration.Tests.resources.south_africa-england-26-12-19.json");
-            using StreamReader reader = new StreamReader(s);
-            expected = JsonSerializer.Deserialize<Match>(reader.ReadToEnd());
         }
 
-        [TestMethod]
-        public async Task GetMatchFixture()
+        [DataTestMethod]
+        [DataRow("Cricinfo.Integration.Tests.resources.south_africa-england-26-12-19.json")]
+        [DataRow("Cricinfo.Integration.Tests.resources.england-australia-6-9-20.json")]
+        [DataRow("Cricinfo.Integration.Tests.resources.england-pakistan-1-9-20.json")]
+        public async Task GetMatchFixture(string filepath)
         {
-            var actual = await cricInfoQueryService.GetMatchAsync(1);
+            using Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream(filepath);
+            using StreamReader reader = new StreamReader(s);
+            var expected = JsonSerializer.Deserialize<Match>(reader.ReadToEnd());
+
+            if (await cricInfoQueryService.MatchExistsAsync(expected.HomeTeam, expected.AwayTeam, expected.DateOfFirstDay))
+            {
+                await cricInfoCommandService.DeleteMatchAsync(expected.HomeTeam, expected.AwayTeam, expected.DateOfFirstDay);
+            }
+
+            var creationResponse = await cricInfoCommandService.CreateMatchAsync(expected);
+
+            Assert.AreEqual(creationResponse.Item1, DataCreationResponse.Success);
+            Assert.IsNotNull(creationResponse.Item2);
+
+            var actual = await cricInfoQueryService.GetMatchAsync((int)creationResponse.Item2);
 
             Assert.AreEqual(expected.Venue, actual.Venue);
             Assert.AreEqual(expected.MatchType, actual.MatchType);
@@ -40,8 +53,12 @@ namespace Cricinfo.Integration.Tests
             Assert.AreEqual(expected.AwayTeam, actual.AwayTeam);
             Assert.AreEqual(expected.Result, actual.Result);
 
-            CollectionAssert.AreEqual(expected.HomeSquad, actual.HomeSquad);
-            CollectionAssert.AreEqual(expected.AwaySquad, actual.AwaySquad);
+            CollectionAssert.AreEqual(
+                expected.HomeSquad.Select(p => p.Replace("(wk)", "").Replace("(c)", "")).OrderBy(p => p).ToArray(),
+                actual.HomeSquad.OrderBy(p => p).ToArray());
+            CollectionAssert.AreEqual(
+                expected.AwaySquad.Select(p => p.Replace("(wk)", "").Replace("(c)", "")).OrderBy(p => p).ToArray(),
+                actual.AwaySquad.OrderBy(p => p).ToArray());
 
             Assert.AreEqual(expected.Scores.Length, actual.Scores.Length);
             foreach (var (e, a) in expected.Scores.Zip(actual.Scores))
@@ -53,16 +70,20 @@ namespace Cricinfo.Integration.Tests
 
                 CollectionAssert.AreEqual(e.FallOfWicketScorecard, a.FallOfWicketScorecard);
 
+                var mappedNames = Parser.Parse.parseNames(actual.HomeSquad.Union(actual.AwaySquad))
+                    .ToDictionary(p => p.Item3, p => string.Concat(p.Item1, " ", p.Item2));
+                mappedNames.Add("sub", null);
+
                 Assert.AreEqual(e.BattingScorecard.Length, a.BattingScorecard.Length);
                 foreach (var (ebs, abs) in e.BattingScorecard.Zip(a.BattingScorecard))
                 {
                     Assert.AreEqual(ebs.Balls, abs.Balls);
-                    Assert.AreEqual(ebs.Bowler, abs.Bowler);
-                    Assert.AreEqual(ebs.Catcher, abs.Catcher);
+                    Assert.AreEqual(ebs.Bowler != null ? mappedNames[ebs.Bowler] : null, abs.Bowler);
+                    Assert.AreEqual(ebs.Catcher != null ? mappedNames[ebs.Catcher] : null, abs.Catcher);
                     Assert.AreEqual(ebs.Dismissal, abs.Dismissal);
                     Assert.AreEqual(ebs.Fours, abs.Fours);
                     Assert.AreEqual(ebs.Mins, abs.Mins);
-                    Assert.AreEqual(ebs.Name, abs.Name);
+                    Assert.AreEqual(mappedNames[ebs.Name], abs.Name);
                     Assert.AreEqual(ebs.Runs, abs.Runs);
                     Assert.AreEqual(ebs.Sixes, abs.Sixes);
                 }
@@ -70,7 +91,7 @@ namespace Cricinfo.Integration.Tests
                 Assert.AreEqual(e.BowlingScorecard.Length, a.BowlingScorecard.Length);
                 foreach (var (ebs, abs) in e.BowlingScorecard.Zip(a.BowlingScorecard))
                 {
-                    Assert.AreEqual(ebs.Name, abs.Name);
+                    Assert.AreEqual(mappedNames[ebs.Name], abs.Name);
                     Assert.AreEqual(ebs.Maidens, abs.Maidens);
                     Assert.AreEqual(ebs.Overs, abs.Overs);
                     Assert.AreEqual(ebs.Runs, abs.Runs);
